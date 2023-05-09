af22705ee7d64d326b3434aa227a0edf7003853d
ranerlich-johnbrice <ranerlich@gmail.com>
2023-05-09 17:51:20 +0300

    added basic flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    print("hello")
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9000)
