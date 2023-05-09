92fe8ceab09c3bd853f2a1bd7379541dae7df983
ranerlich-johnbrice <ranerlich@gmail.com>
2023-05-09 19:43:20 +0300

    added news template

--- news.py
+++ news.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 news_bp = Blueprint('news', __name__,url_prefix='/news')
 
 @news_bp.route('/')
 def news():
-    return "<p> **** This is the NEWS ****</p>"
+    return render_template("news.html")
+    # <p> **** This is the NEWS ****</p>"
 
 @news_bp.route('/global')
 def news_global():
