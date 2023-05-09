dbd319a6d540542c4468abcb75b10069f03c0248
ranerlich-johnbrice <ranerlich@gmail.com>
2023-05-09 17:59:48 +0300

    added route sport and news

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     print("hello")
     return "<p>Hello, World!</p>"
 
+@app.route("/news")
+def news():
+    return "<p>This is the NEWS</p>"
+
+@app.route("/sport")
+def sport():
+    return "<p>This is the SPORT section</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
